2982696e7fa8fcff6f94aa1d334e7d4f918919d2
zhaqj12 <zhaqj12@naver.com>
2023-08-16 16:07:29 +0900

    .
    .

--- site.config.ts
+++ site.config.ts
@@ -42,12 +42,9 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
-
+  pageUrlOverrides: {
+    '/learn': 'ae9f1dae7e6248ac93482aa42cb04af0',
+  },
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   // navigationStyle: 'default'
